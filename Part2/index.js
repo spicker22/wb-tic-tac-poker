1ca6f8419d7860a06770117a303f316baa0b2d93
D H <daniel.f.hyatt@gmail.com>
2023-11-09 16:30:51 -0700

    saving latest2

--- Part2/index.js
+++ Part2/index.js
@@ -4,4 +4,25 @@
 
 
 
-document.querySelector
+
+
+
+const idInput = document.querySelectorAll('#idInput');
+const colorInput = document.querySelectorAll('#colorInput');
+console.log(idInput);
+console.log(colorInput);
+
+
+//// Target Cards /////
+
+const setCards = () => {
+    const card = document.querySelector( `#${idInput.value}`)
+    card.style.color = colorInput.value;
+    console.log(card); 
+}
+
+document.querySelector('#editCardButton').addEventListener('click',setCards)
+
+
+
+
